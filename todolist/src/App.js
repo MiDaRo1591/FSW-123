681cb58868338d511b6720b55bdda451e330e834
Micah <Micah.Ross@bryanuniversity.edu>
2021-11-24 20:51:37 -0600

    ToDoList

---
+++ todolist/src/App.js
@@ -0,0 +1,13 @@
+import ToDoList from './ToDoList.js';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todolist/src/ToDoList.js
@@ -0,0 +1,17 @@
+function ToDoList() {
+    return (
+      <div className="ToDoList">
+        List of ToDos
+        <br></br>
+        <div>
+        <input type='checkbox'></input> Go to Gym<br></br>
+        <input type='checkbox'></input> Do Laundry<br></br>
+        <input type='checkbox'></input> Food Shopping<br></br>
+        <input type='checkbox'></input> Do homework
+        </div>
+      </div>
+    );
+  }
+  
+  export default ToDoList;
+  
